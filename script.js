856bce2a67b85558632bac085e1909d652fa3d7f
FATCullen <98052707+FATCullen@users.noreply.github.com>
2023-07-18 12:12:54 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("nav01").innerHTML =
+"<ul id='menu'>" +
+"<li><a href='index.html'>Home</a></li>" +
+"<li><a href='rules.html'>Rules</a></li>" +
+"<li><a href='opening.html'>Opening</a></li>" +
+"<li><a href='mid.html'>Midgame</a></li>" +
+"<li><a href='end.html'>Endgame</a></li>" +
+"</ul>";
